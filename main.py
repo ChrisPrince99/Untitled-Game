7c3e1236c0b4d7f59f9800f9aa1c0221f5ffcc86
ChrisPrince99 <53632322+ChrisPrince99@users.noreply.github.com>
2019-11-03 22:56:31 +1000

    add main.kv framework, add new_game popup to main.kv, add press_save and press_load methods  respectively, add on_stop method, add clear_fields method, add press_cancel method

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ __author__ = "Christopher J Prince"
 
 import os
 import sys
+import logging
 import world_building
 from kivy.resources import resource_add_path, resource_find
 from kivy.app import App
@@ -19,12 +20,17 @@ from kivy.properties import StringProperty
 from kivy.properties import ListProperty
 from kivy.properties import BooleanProperty, ObjectProperty
 from kivy.core.window import Window
+from datetime import datetime
 from Classes import Player
 from Classes import BackPack
 from Classes import StashBox
 
 
 class UntitledGameApp(App):
+    current_scene = StringProperty()
+    DATE = datetime.now()
+    LOG_FILENAME = "{}.log".format(DATE)
+    logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 
     def __init__(self, **kwargs):
         """
@@ -39,24 +45,56 @@ class UntitledGameApp(App):
         """
         self.title = "Untitled Game"
         self.root = Builder.load_file('main.kv')
+        self.current_scene = "Welcome to my game please choose either New game or Load game."
         return self.root
 
-    def press_new_game(self, name, save_name):
+    def press_new_game(self):
+        self.root.ids.new_game_choice.open()
+
+    def press_start_game(self, added_name, save_name):
         self.root.ids.game_box.clear_widgets()
-        player = Player(name)
-        player_backpack = BackPack("Basic Bag", 10)
-        player_stash = StashBox("Chest", 20)
-        player.save_game(save_name, player, player_backpack, player_stash)
+        self.player = Player(added_name)
+        self.player_backpack = BackPack("Basic Bag", 10)
+        self.player_stash = StashBox("Chest", 20)
+        self.player.save_game(save_name, self.player, self.player_backpack, self.player_stash)
+        self.root.ids.popup.dismiss()
+        self.clear_fields()
 
-    def press_load(self):
-        pass
+    def press_load(self, save_name):
+        self.player.load_game(save_name, self.player, self.player_backpack, self.player_stash)
 
-    def press_save(self):
-        pass
+    def press_save(self, save_name):
+        self.player.save_game(save_name, self.player, self.player_backpack, self.player_stash)
+
+    def on_stop(self):
+        try:
+            self.player.save_game("Autosave", self.player, self.player_backpack, self.player_stash)
+        except Exception as e:
+            logging.exception("main crashed. Error: %s", e)
+
+    def clear_fields(self):
+        """
+        Clear the text input fields from the add entry popup
+        If we don't do this, the popup will still have text in it when opened again
+        :return: None
+        """
+        self.root.ids.added_name.text = ""
+        self.root.ids.added_number.text = ""
+
+    def press_cancel(self):
+        """
+        Handler for pressing cancel in the add entry popup
+        :return: None
+        """
+        self.root.ids.popup.dismiss()
+        self.clear_fields()
+        # self.status_text = ""
 
 
 if __name__ == '__main__':
     if hasattr(sys, '_MEIPASS'):
         resource_add_path(os.path.join(sys._MEIPASS))
-
-    UntitledGameApp().run()
+    try:
+        UntitledGameApp().run()
+    except Exception as e:
+        logging.exception("main crashed. Error: %s", e)
